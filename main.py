132c2a5081355723924a774f9e5c0f468692578e
Matiwos Birbo <workubirbo@gmail.com>
2019-05-25 15:07:54 -0500

    made change

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-
 db = SQLAlchemy(app)
+app.secret_key = 'bepro'
 
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name,owner):
         self.name = name 
         self.completed = False
-
+        self.owner = owner
 
 class User(db.Model):
     id = db.Column(db.Integer, primery_key = True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        redirect('/login')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -35,11 +44,12 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(email=email).first()
         if user and user.password == password:
-            # TODO - "remember" that the user has logged in
+            session['email'] = email
+            flash("Logged in")
+            print(session)
             return redirect('/')
         else:
-            #TODO - explain why login failed
-            return '<h1>Error!</h1>'
+           flash('User password incorrect, or user does not existS', 'error')
 
     return render_template('login.html')
 
@@ -57,7 +67,7 @@ def register():
             new_user = User(email, password)
             db.session.add(new_user)
             db.session.commit()
-            # TODO - "remember" the user
+            session['email'] = email
             return redirect('/')
         else:
             #TODO - user better response messaging
@@ -65,18 +75,26 @@ def register():
         
     return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
  
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
 
 
-    tasks = Task.query.filter_by(completed = False).all()
-    completed_tasks = Task.query.filter_by(completed = False).all()
+    tasks = Task.query.filter_by(completed = False, owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed = True, owner=owner).all()
     return render_template('todos.html', title="Get It Done!", tasks=tasks, 
     completed_tasks=completed_tasks)
 
