40e1846a2df357acdae869b0d05d278ad77483be
Matiwos Birbo <workubirbo@gmail.com>
2019-05-26 04:30:29 -0500

    made change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,6 +9,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'bepro'
 
+
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -15,27 +17,30 @@ class Task(db.Model):
     completed = db.Column(db.Boolean)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name,owner):
-        self.name = name 
+    def __init__(self, name, owner):
+        self.name = name
         self.completed = False
         self.owner = owner
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primery_key = True)
+    id = db.Column(db.Integer, primery_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    
+
     tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
 
+
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'register']
     if request.endpoint not in allowed_routes and 'email' not in session:
         redirect('/login')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -49,9 +54,10 @@ def login():
             print(session)
             return redirect('/')
         else:
-           flash('User password incorrect, or user does not existS', 'error')
+            flash('User password incorrect, or user does not existS', 'error')
 
     return render_template('login.html')
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -59,9 +65,29 @@ def register():
         email = request.form['email']
         password = request.form['password']
         verify = request.form['verify']
-       
-        #TODO - validate user's date
-        
+
+    if username == "":
+        usernameError = "Please enter a valid username."
+    elif len(username) <= 3 or len(username) > 20:
+        usernameError = ""
+    elif " " in username:
+        usernameError = "Your username cannot contain any spaces."
+        username = ""
+
+    if password == "":
+        passwordError = "Please enter a valid password."
+    elif len(password) < 3 or len(password) > 20:
+        passwordError = "Password must be at least 3 to 20 characters long."
+    elif " " in password:
+        passwordError = "Your cannot contain any spaces"
+
+    if verify_password == "" or verify_password != password:
+        verify = "Password do not match. Please try again."
+        verify = ""
+    if email != "":
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-]+\.[a-zA-Z0-9-.]+$)", email):
+            emailError = "Not a valid email address."
+
         existing_user = User.query.filter_by(email=email).first()
         if not existing_user:
             new_user = User(email, password)
@@ -70,17 +96,18 @@ def register():
             session['email'] = email
             return redirect('/')
         else:
-            #TODO - user better response messaging
+            # TODO - user better response messaging
             return "<h1> Duplicate user</h1>"
-        
+
     return render_template('register.html')
+
 
 @app.route('/logout')
 def logout():
     del session['email']
     return redirect('/')
 
- 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
@@ -92,11 +119,10 @@ def index():
         db.session.add(new_task)
         db.session.commit()
 
-
-    tasks = Task.query.filter_by(completed = False, owner=owner).all()
-    completed_tasks = Task.query.filter_by(completed = True, owner=owner).all()
-    return render_template('todos.html', title="Get It Done!", tasks=tasks, 
-    completed_tasks=completed_tasks)
+    tasks = Task.query.filter_by(completed=False, owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True, owner=owner).all()
+    return render_template('todos.html', title="Get It Done!", tasks=tasks,
+                           completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
@@ -109,6 +135,7 @@ def delete_task():
     db.session.commit()
 
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
